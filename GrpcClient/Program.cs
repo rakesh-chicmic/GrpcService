7f69083900eaaab9b551b181afc2431f6f66cb97
Rakesh Kumar <rakesh.kumar23@chicmic.co.in>
2023-06-21 11:18:45 +0530

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7047");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(
+    new HelloRequest { Name = "World" });
+
+Console.WriteLine(response.Message);
+
+var client2 = new Product.ProductClient(channel);
+var product = new GetProductDetails { ProductId = 2 };
+var response2 = await client2.GetProductsInformationAsync(product);
+
+Console.WriteLine(response2);
+
+
